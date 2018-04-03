0217dac344a90db2a3735a06280be92d6e5c32ae
ulitpengkul <ulitpengkul@gmail.com>
2018-04-03 15:06:12 +0700

    006

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+
+require_once('./vendor/autoload.php');
+// Namespace
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+$channel_token = 's2YsG6Cf/nk3Yxuc+hNhlFzPx1ok9XEcIAY5eZdCLzVQUJr87DHxI/qBlzwiXP9dhNj+ujLeu0J/ClG21vs8xplEjvJ3BwVxc0+T0Po8IIXaRA0GqM6Bbij9G6SUdhEQb18iLwFuiPyHWw8Eja+fEwdB04t89/1O/w1cDnyilFU=';
+$channel_secret = 'a5c0adcfff271a2f3235f7a50033de3e';
+
 echo "Test AAA";
 ?>
